fcedefe4466351862cf9290dac7dab7f6501dfd7
Paul Schifferer <paul@schifferers.net>
2024-11-13 19:35:51 -0800

    Update test

--- Tests/ModelCoreTests/ModelCoreTests.swift
+++ Tests/ModelCoreTests/ModelCoreTests.swift
@@ -1,6 +1,5 @@
-import Testing
+import XCTest
 @testable import ModelCore
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+func testExample() throws {
 }
